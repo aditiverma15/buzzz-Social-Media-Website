fbbcce4550a74aea49a1a4f1917938c00d5eef54
aditiverma15 <63070359+aditiverma15@users.noreply.github.com>
2022-04-14 14:21:13 +0530

    Add files via upload

---
+++ user-profile/src/components/User.jsx
@@ -0,0 +1,43 @@
+import React from "react";
+import './index.css';
+export default function Profile() {
+    return (
+      <>
+    
+        <div className="wrapper">
+    
+         
+            <div className="left">
+                <div className="post">
+              <div className="profileCover">
+                <img
+                  className="cover-image"
+                  src="assets/3.jpeg"
+                  alt=""
+                />
+                <img
+                  className="profileUserImg"
+                  src="assets/7.jpeg"
+                  alt=""
+                />
+              </div>
+              <div className="profileInfo">
+                  <h1 className="profileInfoName">Sarah Wood</h1>
+                  <p className="profileInfoDesc">Sarah Wood is co-founder and COO of video ad tech company</p>
+                  <p><i className="sub-text">London . England . United Kingdom . 234 Friends</i></p><br />
+                  <button className="button1"> <span className="text">Add Friends</span></button>
+                  <button className="button2"> <span className="text2">Visit Website</span></button>
+                  
+                  
+              </div>
+              </div>
+            </div>
+            <div className="right"><div className="side-bar">fdytfydagcdu</div>
+          </div>
+            
+          </div>
+        
+      </>
+    );
+  }
+  
